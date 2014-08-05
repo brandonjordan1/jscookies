419cb842017ab2fa36e7309371cfcbf290743a78
Brandon <brandon_jordan360@yahoo.com>
2014-08-05 10:26:11 -0400

    Update jscookies.js

--- jscookies.js
+++ jscookies.js
@@ -1,31 +1,19 @@
-function setCookie(c_name,value,exdays)
+function setCookie(cname,cvalue,exdays)
 {
-var exdate=new Date();
-exdate.setDate(exdate.getDate() + exdays);
-var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-document.cookie=c_name + "=" + c_value;
+var d = new Date();
+d.setTime(d.getTime()+(exdays*24*60*60*1000));
+var expires = "expires="+d.toGMTString();
+document.cookie = cname + "=" + cvalue + "; " + expires;
 }
-function getCookie(c_name)
+
+function getCookie(cname)
 {
-var c_value = document.cookie;
-var c_start = c_value.indexOf(" " + c_name + "=");
-if (c_start == -1)
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) 
   {
-  c_start = c_value.indexOf(c_name + "=");
-  }
-if (c_start == -1)
-  {
-  c_value = null;
-  }
-else
-  {
-  c_start = c_value.indexOf("=", c_start) + 1;
-  var c_end = c_value.indexOf(";", c_start);
-  if (c_end == -1)
-  {
-c_end = c_value.length;
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
 }
-c_value = unescape(c_value.substring(c_start,c_end));
-}
-return c_value;
+return "";
 }
